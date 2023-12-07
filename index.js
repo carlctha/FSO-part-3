0611945642cb98bd95d2c7eb25a48ad4f58b6b3f
Carl <carlctha1@gmail.com>
2023-12-07 16:04:39 +0100

    Exercise 3.5

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
+const randomInt = () => Math.floor(Math.random() * 1000000)
+
 let persons = [
     { 
       "id": 1,
@@ -53,11 +57,41 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id);
-    persons = persons.filter(p => p.id !== id)
+    persons = persons.filter(p => p.id !== id);
 
-    console.log(persons)
-    
-    res.status(204).end()
+    console.log(persons);
+
+    res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body;
+
+    if (!body.name && !body.number) {
+        return res.status(400).json({
+            error: "You need name and number"
+        });
+    }
+    else if (!body.name) {
+        return res.status(400).json({
+            error: "You need to add name"
+        });
+    }
+    else if (!body.number) {
+        return res.status(400).json({
+            error: "You need to add number"
+        });
+    };
+
+    const person = {
+        id: randomInt(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
 })
 
 const PORT = 3001;
